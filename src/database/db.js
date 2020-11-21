7bf4ef3d71c0b3c4f19979d258bdc275665fe062
Rian dos Passos <riandpf2002@gmail.com>
2020-11-21 20:45:20 -0300

    .

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("blog_curso", "rian-dpf", "rian080920022019", {
-  host: "mysql741.umbler.com",
+const connection = new Sequelize("blog", "root", "123456", {
+  host: "localhost",
   dialect: "mysql",
   timezone: "-03:00",
 });
